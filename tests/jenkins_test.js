30e82b2f9b544f4227f000c1ec64e067e26a0e4e
TevfikY <92394864+TevfikY@users.noreply.github.com>
2023-02-21 13:36:31 +0300

    ...

--- tests/jenkins_test.js
+++ tests/jenkins_test.js
@@ -2,9 +2,9 @@ Feature('jenkins');
 
 Before(({I})=> {
 
-        I.amOnPage("https://www.facebook.com/");
+        I.amOnPage("https://www.trendyol.com/");
 })
 
 Scenario('test something',  ({ I }) => {
-        I.seeCurrentUrlEquals("https://www.facebook.com/");
+        I.seeCurrentUrlEquals("https://www.trendyol.com/");
 });
