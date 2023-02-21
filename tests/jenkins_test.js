188f04cfb70d440879b7a12ae514a86c6f39c109
TevfikY <92394864+TevfikY@users.noreply.github.com>
2023-02-21 13:42:06 +0300

    Update jenkins_test.js

--- tests/jenkins_test.js
+++ tests/jenkins_test.js
@@ -7,6 +7,6 @@ Before(({I})=> {
 
 Scenario('test something',  ({ I }) => {
         I.seeCurrentUrlEquals("https://www.trendyol.com/");
-        I.waitForNavigation("//*[@class='tab-link']/following-sibling::*[3]");
+        I.waitForElement("//*[@class='tab-link']/following-sibling::*[3]");
         I.click("//*[@class='tab-link']/following-sibling::*[3]");
 });
